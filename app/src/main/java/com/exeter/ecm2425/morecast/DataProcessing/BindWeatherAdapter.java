420ce9f2fa1d1b2a58bf61bd2cc5a41c4b53eaf2
James <jrmd202@exeter.ac.uk>
2018-03-23 16:40:20 +0000

    Refactored out a binding class for the WeatherAdapter

---
+++ app/src/main/java/com/exeter/ecm2425/morecast/DataProcessing/BindWeatherAdapter.java
@@ -0,0 +1,97 @@
+package com.exeter.ecm2425.morecast.DataProcessing;
+
+
+import com.exeter.ecm2425.morecast.Database.FiveDayForecast;
+import com.exeter.ecm2425.morecast.Utils.DateHandler;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class BindWeatherAdapter {
+    private ArrayList<FiveDayForecast> dayForecast;
+    private WeatherAdapter.ViewHolder viewHolder;
+
+
+    public BindWeatherAdapter
+            (ArrayList<FiveDayForecast> forecasts, WeatherAdapter.ViewHolder holder) {
+        this.dayForecast = forecasts;
+        this.viewHolder = holder;
+    }
+
+    void bindToday() {
+        bindMainInformation();
+        bindAdditionalInformation();
+        bindImageToday();
+        bindLabels();
+    }
+
+    void bindForecast() {
+        bindForecastInformation();
+        bindImageForecasts();
+    }
+
+    private void bindMainInformation() {
+        FiveDayForecast closestTime = dayForecast.get(0);
+        double temp = closestTime.getTemperature();
+        String description = closestTime.getDescription();
+        viewHolder.todayView.setMainInfo(temp, description);
+    }
+
+    private void bindAdditionalInformation() {
+        FiveDayForecast closestTime = dayForecast.get(0);
+        double pressure = closestTime.getPressure();
+        int humidity = closestTime.getHumidity();
+        double windSpeed = closestTime.getWindSpeed();
+        double windDirection = closestTime.getWindDegree();
+        String precipType = closestTime.getPrecipitationType();
+        double precipAmount = closestTime.getPrecipitationAmount();
+
+        viewHolder.todayView.setAdditionalWeatherInfo(
+                pressure, humidity, windSpeed, windDirection, precipType, precipAmount
+        );
+    }
+
+    private void bindForecastInformation() {
+        FiveDayForecast midDay = dayForecast.get(4);
+        long timestamp = midDay.getEpochTime();
+        Date currentDate = new Date(timestamp * 1000);
+
+        String currentDay = DateHandler.returnDayOfTheWeek(currentDate);
+        double temp = midDay.getTemperature();
+        viewHolder.forecastView.setForecast(currentDay, temp);
+    }
+
+    private void bindImageToday() {
+        FiveDayForecast closestTime = dayForecast.get(0);
+        FiveDayForecast secondTime = dayForecast.get(1);
+        FiveDayForecast thirdTime = dayForecast.get(2);
+        FiveDayForecast fourthTime = dayForecast.get(3);
+
+        int firstCode = closestTime.getWeatherCode();
+        int secondCode = secondTime.getWeatherCode();
+        int thirdCode = thirdTime.getWeatherCode();
+        int fourthCode = fourthTime.getWeatherCode();
+        int currentHour = DateHandler.getLocaleHour();
+        viewHolder.todayView.setImages(firstCode, secondCode, thirdCode, fourthCode, currentHour);
+    }
+
+    private void bindImageForecasts() {
+        FiveDayForecast midDay = dayForecast.get(4);
+        int code = midDay.getWeatherCode();
+        int currentHour = DateHandler.getLocaleHour();
+        viewHolder.forecastView.setForecastImage(code, currentHour);
+    }
+
+    private void bindLabels() {
+        FiveDayForecast firstForecast = dayForecast.get(0);
+        FiveDayForecast secondForecast = dayForecast.get(1);
+        FiveDayForecast thirdForecast = dayForecast.get(2);
+        FiveDayForecast fourthForecast = dayForecast.get(3);
+
+        String firstTime = firstForecast.getDateTime();
+        String secondTime = secondForecast.getDateTime();
+        String thirdTime = thirdForecast.getDateTime();
+        String fourthTime = fourthForecast.getDateTime();
+        viewHolder.todayView.setLabels(firstTime, secondTime, thirdTime, fourthTime);
+    }
+}
--- app/src/main/java/com/exeter/ecm2425/morecast/DataProcessing/WeatherAdapter.java
+++ app/src/main/java/com/exeter/ecm2425/morecast/DataProcessing/WeatherAdapter.java
@@ -46,7 +46,7 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
         fiveDayForecasts = forecasts;
     }
 
-    // get next day, if it cant bind the next four times, get n-1 position from next day.
+
     @NonNull
     @Override
     public WeatherAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
@@ -69,16 +69,14 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
         ArrayList<FiveDayForecast> day;
         if(holder.getItemViewType() == 0) {
             day = ResultParser.getForecastDay(fiveDayForecasts);
-            bindMainInformation(day, holder);
-            bindAdditionalInformation(day, holder);
-            bindImageToday(day, holder);
-            bindLabels(day, holder);
+            BindWeatherAdapter binder = new BindWeatherAdapter(day, holder);
+            binder.bindToday();
         }
 
         else {
             day = ResultParser.getForecastDay(fiveDayForecasts);
-            bindForecastInformation(day, holder);
-            bindImageForecasts(day, holder);
+            BindWeatherAdapter binder = new BindWeatherAdapter(day, holder);
+            binder.bindForecast();
         }
     }
 
@@ -92,71 +90,5 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
     @Override
     public int getItemCount() {
         return FORECAST_DAYS;
-    }
-
-
-    private void bindMainInformation(ArrayList<FiveDayForecast> day, @NonNull ViewHolder holder) {
-        FiveDayForecast closestTime = day.get(0);
-        double temp = closestTime.getTemperature();
-        String description = closestTime.getDescription();
-        holder.todayView.setMainInfo(temp, description);
-    }
-
-    private void bindAdditionalInformation(ArrayList<FiveDayForecast> day, @NonNull ViewHolder holder) {
-        FiveDayForecast closestTime = day.get(0);
-        double pressure = closestTime.getPressure();
-        int humidity = closestTime.getHumidity();
-        double windSpeed = closestTime.getWindSpeed();
-        double windDirection = closestTime.getWindDegree();
-        String precipType = closestTime.getPrecipitationType();
-        double precipAmount = closestTime.getPrecipitationAmount();
-
-        holder.todayView.setAdditionalWeatherInfo(
-                pressure, humidity, windSpeed, windDirection, precipType, precipAmount
-        );
-    }
-
-    private void bindForecastInformation(ArrayList<FiveDayForecast> day, @NonNull ViewHolder holder) {
-        FiveDayForecast midDay = day.get(4);
-        long timestamp = midDay.getEpochTime();
-        Date currentDate = new Date(timestamp * 1000);
-
-        String currentDay = DateHandler.returnDayOfTheWeek(currentDate);
-        double temp = midDay.getTemperature();
-        holder.forecastView.setForecast(currentDay, temp);
-    }
-
-    private void bindImageToday(ArrayList<FiveDayForecast> day, @NonNull ViewHolder holder) {
-        FiveDayForecast closestTime = day.get(0);
-        FiveDayForecast secondTime = day.get(1);
-        FiveDayForecast thirdTime = day.get(2);
-        FiveDayForecast fourthTime = day.get(3);
-
-        int firstCode = closestTime.getWeatherCode();
-        int secondCode = secondTime.getWeatherCode();
-        int thirdCode = thirdTime.getWeatherCode();
-        int fourthCode = fourthTime.getWeatherCode();
-        int currentHour = DateHandler.getLocaleHour();
-        holder.todayView.setImages(firstCode, secondCode, thirdCode, fourthCode, currentHour);
-    }
-
-    private void bindImageForecasts(ArrayList<FiveDayForecast> day, @NonNull ViewHolder holder) {
-        FiveDayForecast midDay = day.get(4);
-        int code = midDay.getWeatherCode();
-        int currentHour = DateHandler.getLocaleHour();
-        holder.forecastView.setForecastImage(code, currentHour);
-    }
-
-    private void bindLabels(ArrayList<FiveDayForecast> day, @NonNull ViewHolder holder) {
-        FiveDayForecast firstForecast = day.get(0);
-        FiveDayForecast secondForecast = day.get(1);
-        FiveDayForecast thirdForecast = day.get(2);
-        FiveDayForecast fourthForecast = day.get(3);
-
-        String firstTime = firstForecast.getDateTime();
-        String secondTime = secondForecast.getDateTime();
-        String thirdTime = thirdForecast.getDateTime();
-        String fourthTime = fourthForecast.getDateTime();
-        holder.todayView.setLabels(firstTime, secondTime, thirdTime, fourthTime);
     }
 }
