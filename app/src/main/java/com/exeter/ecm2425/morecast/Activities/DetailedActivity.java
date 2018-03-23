4a5431bc5e00dc290b14eee8fc94a337f6d5e17f
James <jrmd202@exeter.ac.uk>
2018-03-23 17:57:47 +0000

    Added default activity with custom view, needs click listenrs for recyclerview to pass current data to them for binding

---
+++ app/src/main/java/com/exeter/ecm2425/morecast/Activities/DetailedActivity.java
@@ -0,0 +1,15 @@
+package com.exeter.ecm2425.morecast.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.exeter.ecm2425.morecast.R;
+
+public class DetailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+    }
+}
